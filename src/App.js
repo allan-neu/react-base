68f94ae62010426200b20cda642926610274440c
allan-neu <allan@itsneu.com>
2017-06-09 14:06:05 +0100

    style.app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
-      <div className="App">
+      <div className={styles.App}>
         {this.props.children}
       </div>
     );
